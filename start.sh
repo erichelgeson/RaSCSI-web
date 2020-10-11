7cb1a50ec97f8857d8dfb63da8e258b5ba8adb31
Eric Helgeson <erichelgeson@gmail.com>
2020-10-11 14:37:33 -0500

    Make start a little more safe

--- start.sh
+++ start.sh
@@ -2,17 +2,32 @@
 set -e
 
 # verify packages installed
-# python3-venv, genisoimage, etc
+if ! command -v genisoimage &> /dev/null ; then
+    echo "genisoimage could not be found"
+    echo "Run 'sudo apt install genisoimage' to fix."
+    exit 1
+fi
+if ! command -v python3 &> /dev/null ; then
+    echo "python3 could not be found"
+    echo "Run 'sudo apt install python3' to fix."
+    exit 1
+fi
+if ! python3 -m venv --help &> /dev/null ; then
+    echo "venv could not be found"
+    echo "Run 'sudo apt install python3-venv' to fix."
+    exit 1
+fi
 
 if ! test -e venv; then
   echo "Creating python venv for web server"
   python3 -m venv venv
+  echo "Activating venv"
+  source venv/bin/activate
+  echo "Installing requirements.txt"
+  pip install -r requirements.txt
 fi
 
 source venv/bin/activate
-
-#echo "Checking all requirements are installed"
-#pip install -r requirements.txt
 
 echo "Starting web server..."
 python3 web.py
