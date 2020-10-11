1d72600beba9d65de601ee7ba0f4abd36cbaa9b2
Eric Helgeson <erichelgeson@gmail.com>
2020-10-11 11:13:06 -0500

    few minor updates to start

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
+set -e
 
 # verify packages installed
 # python3-venv, iso utility, etc
 
 if ! test -e venv; then
+  echo "Creating python venv for web server"
   python3 -m venv venv
 fi
 
 source venv/bin/activate
+
+echo "Checking all requirements are installed"
 pip install -r requirements.txt
 
+echo "Starting web server..."
 python3 web.py
